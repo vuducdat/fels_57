9c16492c672f008c7b71bce50f91f9e45a1871a1
vuducdat <bka.dat@gmail.com>
2015-06-16 15:57:48 +0700

    Design login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == Settings.remember ? remember(user) : forget(user) 
+      redirect_to user
+    else
+      flash.now[:danger] = t "danger.errorMail"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ActiveRecord::Base
   has_many :lessons, dependent: :destroy
   has_many :activities, dependent: :destroy
-  has_many :active_relationships,  class_name:  "Relationship",
+  has_many :active_relationships, class_name: "Relationship",
     foreign_key: "follower_id",
     dependent: :destroy
-  has_many :passive_relationships, class_name:  "Relationship",
+  has_many :passive_relationships, class_name: "Relationship",
     foreign_key: "followed_id",
     dependent: :destroy                                
-  has_many :following, through: :active_relationships,  source: :followed
+  has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  attr_accessor :remember_token
 
   before_save {self.email = email.downcase}
   
@@ -18,4 +20,23 @@ class User < ActiveRecord::Base
   format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false} 
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil? 
+    BCrypt::Password.new remember_digest.is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
 end
