b9381e17ecd3d450cc4da74f63829fd78d98034c
vuducdat <bka.dat@gmail.com>
2015-06-17 09:59:47 +0700

    Update Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
   has_many :lessons, dependent: :destroy
   has_many :activities, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
@@ -10,16 +12,15 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-  attr_accessor :remember_token
-
   before_save {self.email = email.downcase}
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name,  presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255},
-  format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false} 
+  validates :name,  presence: true, length: {maximum: Settings.name_maximum}
+  validates :email, presence: true, length: {maximum: Settings.email_maximum},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false} 
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: Settings.password_maximum_length},
+    allow_nil: true
 
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
