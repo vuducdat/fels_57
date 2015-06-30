2a6a9ac6d1fa6f90de41c52d8e68dd9bf5584a7e
vuducdat <bka.dat@gmail.com>
2015-06-30 17:46:08 +0700

    User can use button Words && button Lessons

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @activities = current_user.following_activities.paginate page: params[:page],
+                                                               per_page: Settings.per_page
+    end
   end
 end
