e8a308f3f70035be87747fefd704f3713cf64230
PhuongND <phuongnd.bk@gmail.com>
2015-07-01 14:36:09 +0700

    Refactor create, edit word form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 
 function remove_fields(link) {
   $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".fields").hide();
+  $(link).closest(".field").hide();
 }
 
 function add_fields(link, association, content) {
